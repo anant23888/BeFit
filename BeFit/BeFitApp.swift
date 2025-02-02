393fb01aa390ac4d0cd8d37ccd3e0e20fcb8f68c
CHATURVEDI ANANT <chaturvedi.a20@iiits.in>
2025-02-02 13:58:59 +0530

    Initial Commit

---
+++ BeFit/BeFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeFitApp.swift
+//  BeFit
+//
+//  Created by Anant Chaturvedi on 02/02/25.
+//
+
+import SwiftUI
+
+@main
+struct BeFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
